33148c88cf92e8f48654832d44cf7f83cd88713a
limao <6043505@qq.com>
2024-09-21 21:39:55 +0800

    初始化图标库2

---
+++ icons.js
@@ -0,0 +1,35 @@
+{
+    "line": {
+      "arrows": [
+        {
+          "name": "Arrow 1",
+          "chineseName": "箭头 1",
+          "category": "arrows",
+          "svgUrl": "https://raw.githubusercontent.com/limao6/icon-library/main/icons/arrow1.svg"
+        },
+        {
+          "name": "Arrow 2",
+          "chineseName": "箭头 2",
+          "category": "arrows",
+          "svgUrl": "https://raw.githubusercontent.com/limao6/icon-library/main/icons/arrow2.svg"
+        }
+      ]
+    },
+    "filled": {
+      "arrows": [
+        {
+          "name": "Filled Arrow 1",
+          "chineseName": "填充箭头 1",
+          "category": "arrows",
+          "svgUrl": "https://raw.githubusercontent.com/limao6/icon-library/main/icons/filled_arrow1.svg"
+        },
+        {
+          "name": "Filled Arrow 2",
+          "chineseName": "填充箭头 2",
+          "category": "arrows",
+          "svgUrl": "https://raw.githubusercontent.com/limao6/icon-library/main/icons/filled_arrow2.svg"
+        }
+      ]
+    }
+  }
+  
